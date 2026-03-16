ca1f1c1f2315a0ffba8e4129de5cb1f31f4f4934
Mirock <kashingeotgii@gmail.com>
2026-03-16 13:56:30 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,9 +1,12 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
 
-int64_t power(int64_t x, uint16_t n) {
+int64_t power(int64_t x, uint16_t n)
+{
     int64t result = 1;
     for (uint16_t i = 1; i <= n; i++)
+        {
         result *= x ;
+        }
     return result;
 }
