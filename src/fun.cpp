2d574cafd6494f718dbad05854bf99397aacbe2e
Mirock <kashingeotgii@gmail.com>
2026-03-16 13:48:36 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -3,7 +3,7 @@
 
 int64_t power(int64_t x, uint16_t n) {
     int64t result = 1;
-    for(uint16_t i = 1; i < n; i++)
+    for (uint16_t i = 1; i <= n; i++)
         result *= x ;
     return result;
 }
